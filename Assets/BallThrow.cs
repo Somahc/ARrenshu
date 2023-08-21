e447dc3770f3795fa9d3f17ceeddde11052f3505
Soma Sekine <sekine.soma.ss@gmail.com>
2023-08-21 14:32:30 +0900

    タップでボールを出す機能実装

---
+++ Assets/BallThrow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrow : MonoBehaviour
+{
+    public GameObject m_prefBall;
+    public Transform m_transformCamera;
+
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Rigidbody rigidbodyBall = Instantiate(m_prefBall).GetComponent<Rigidbody>();
+                rigidbodyBall.transform.position = m_transformCamera.position;
+                rigidbodyBall.velocity = m_transformCamera.forward * 10f;
+            }
+        }
+    }
+}
